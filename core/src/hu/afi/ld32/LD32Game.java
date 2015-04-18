da7bc79f134923da8dba76621d2c4d4bb05562ff
bluegekko <gekko42@gmail.com>
2015-04-18 13:40:19 +0200

    Add basic main menu.

--- core/src/hu/afi/ld32/LD32Game.java
+++ core/src/hu/afi/ld32/LD32Game.java
@@ -1,15 +1,22 @@
 package hu.afi.ld32;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//ApplicationAdapter
 
-public class LD32Game extends ApplicationAdapter {
-	SpriteBatch batch;
+public class LD32Game extends Game {
+	@Override
+	public void create() {
+		setScreen(new MenuScreen(this));
+	}
+
+	/*SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -23,5 +30,5 @@ public class LD32Game extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
-	}
+	}*/
 }
